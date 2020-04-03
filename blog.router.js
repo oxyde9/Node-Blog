24ea1975db1c650a6f655ccb96eab653b21fcce6
Rui <ruiduarte617@gmail.com>
2020-04-03 17:38:00 +0200

    Finish

--- blog.router.js
+++ blog.router.js
@@ -7,7 +7,22 @@ blogRouter.get('/', (req, res)=> {
     const prenom = "Rui";
     res.render('index.pug', {prenom})
 });
-blogRouter.get('/admin', (req, res)=> res.send('Bienvenue'));
+blogRouter.get('/article', (req, res)=> { 
+
+    res.render('article.pug')
+});
+blogRouter.get('/admin', (req, res)=>{ 
+   
+    res.render('admin/admin.pug')
+});
+blogRouter.get('/admin/edit', (req, res)=>{ 
+   
+    res.render('admin/edit.pug')
+});
+blogRouter.get('/admin/write', (req, res)=>{ 
+   
+    res.render('admin/write.pug')
+});
 
 //Export l'objet Router crée
 module.exports = blogRouter;
--- index.js
+++ index.js
@@ -1,20 +1,30 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
-const blogRouter = require('./blog.router');
-app.set('view engine', 'pug');
+const mongoose = require('mongoose');
 
-app.set('views', './views');
+const app = express();
+const blogRouter = require('./blog.routeur');
+
+app.set('view engine', 'pug'); //indique à Express que le moteur de templating à utiliser sera "Pug"
+app.set('views', './views'); //indique à Express le dossier où se trouvent les vues (fichiers .pug)
 
 const PORT = 9000;
 const HOST = 'localhost';
 
-//traite les routes pour la partie front-office
-app.use('/', blogRouter);
+app.use ('/', blogRouter); //traite toutes les routes pour la partie front office
 
-//Demarrage de l'appli
-//---------------------------------
+//démarrage de l'application
+//-----------------------------------
 
-
-
-
-app.listen(PORT, HOST, () => console.log(`Exemple app listening sur  http://${HOST}:${PORT}`));
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+}
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`, options)
+        .then(() => console.log('Mongoose : connexion établie à Atlas !'))
+        .then(() => {
+            app.listen(PORT,HOST, () => {
+                console.log(`Express : le serveur écoute sur http://${HOST}:${PORT}`);
+            })
+        })
+        .catch((err) => console.error(err));
